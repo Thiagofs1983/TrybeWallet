cd75673c65b7c67f3784508a7a38fbcd9b82d7af
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-05-18 16:41:39 -0300

    adiciona componetne Header e configurações iniciais

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { emailProps } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          { `Email: ${emailProps}` }
+        </span>
+        <span data-testid="total-field">
+          0
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  emailProps: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  emailProps: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
