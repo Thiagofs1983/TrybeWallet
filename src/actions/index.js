326a3e6f42e1dc46cf442938ae0195c66591b2cb
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-05-18 16:19:22 -0300

    criar pagina de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+
+export const addEmailAction = (payload) => ({
+  type: ADD_EMAIL,
+  payload,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEmailAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit = () => {
+    const { email } = this.state;
+    const { emailProps, history } = this.props;
+    emailProps(email);
+    history.push('/carteira');
+  };
+
   render() {
+    const validateEmail = /\S+@\S+\.\S+/;
+    const MIN_LENGTH_PASSWORD = 6;
+    const {
+      email,
+      password,
+    } = this.state;
     return (
       <main>
-        form
+        <form action="">
+          <h1>TRYBE WALLET</h1>
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            placeholder="E-mail"
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+          <input
+            type="text"
+            name="password"
+            value={ password }
+            placeholder="Senha"
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+          <button
+            type="button"
+            disabled={
+              password.length < MIN_LENGTH_PASSWORD || !validateEmail.test(email)
+            }
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </form>
       </main>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  emailProps: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  emailProps: (state) => dispatch(addEmailAction(state)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
