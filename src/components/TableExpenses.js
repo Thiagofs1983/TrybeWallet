9d5f92c186b2dfc36c4b57592cbbc0fb0818f1d2
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-05-19 12:20:20 -0300

    implementada a lógica que alimenta a tabela de despesas

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class TableExpenses extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <tbody>
@@ -16,10 +19,43 @@ class TableExpenses extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {
+            expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ parseFloat(expense.value).toFixed(2) }</td>
+                <td>{ (expense.exchangeRates[expense.currency].name).split('/')[0] }</td>
+                <td>
+                  { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+                </td>
+                <td>
+                  {
+                    (parseFloat(expense.value)
+                    * (expense.exchangeRates[expense.currency].ask)).toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     );
   }
 }
 
-export default TableExpenses;
+TableExpenses.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(TableExpenses);
